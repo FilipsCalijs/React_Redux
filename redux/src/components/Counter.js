4e897a06bbb342f4723728c7653d87d8debb31f1
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-04-10 19:14:08 +0300

    create react and redux with connect

--- redux/src/components/Counter.js
+++ redux/src/components/Counter.js
@@ -1,4 +1,7 @@
 
+import { connect } from "react-redux";
+import {inc, dec, rnd} from '../actions';
+
 const Counter = ({counter, inc, dec, rnd}) => {
     return (
         <div className="jumbotron">
@@ -10,4 +13,23 @@ const Counter = ({counter, inc, dec, rnd}) => {
     )
 }
 
-export default Counter;
+const mapStateToProps = (state) => {
+        return {
+            counter: state.value
+        }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        inc: () => dispatch(inc()),
+        dec: () => dispatch(dec()),
+        rnd: () => {
+            const value = Math.floor(Math.random() * 10);
+            dispatch(rnd(value));
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+
+
