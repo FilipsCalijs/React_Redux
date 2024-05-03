46b4701f5506d3948a3fdd374ed13c5ed4492b1c
FilipsCalijs <filipscalijs@gmail.com>
2024-05-03 23:31:44 +0300

    createEntityAdapter done

--- hero_admin_panel/src/components/heroesList/heroesSlice.js
+++ hero_admin_panel/src/components/heroesList/heroesSlice.js
@@ -26,10 +26,10 @@ const heroesSlice = createSlice({
     initialState,
     reducers: {
         heroCreated: (state, action) => {
-            state.heroes.push(action.payload);
+            heroesAdapter.addOne(state, action.payload)
         },
         heroDeleted: (state, action) => {
-            state.heroes = state.heroes.filter(item => item.id !== action.payload);
+            heroesAdapter.removeOne(state, action.payload)
         }
     },
     extraReducers: (builder) => {
@@ -49,6 +49,8 @@ const heroesSlice = createSlice({
 const {actions, reducer} = heroesSlice;
 
 export default reducer;
+
+export const { selectAll } = heroesAdapter.getSelectors(state => state.heroes);
 export const {
     heroesFetching,
     heroesFetched,
